91c6ff3aed9866418edca2177b2fef899f86a2e3
Virtus_Chae <ybd4731@gmail.com>
2024-09-04 14:38:54 +0900

    first commit

---
+++ src/main/java/com/metarealm/metarealm_be/MetarealmBeApplication.java
@@ -0,0 +1,13 @@
+package com.metarealm.metarealm_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetarealmBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetarealmBeApplication.class, args);
+    }
+
+}
