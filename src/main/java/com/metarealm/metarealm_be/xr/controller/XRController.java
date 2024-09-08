a3eef2531808e8d88c160c330899509468b1abb0
Virtus_Chae <ybd4731@gmail.com>
2024-09-08 21:58:47 +0900

    refactor: ResponseEntity 및 로그 추가

--- src/main/java/com/metarealm/metarealm_be/xr/controller/XRController.java
+++ src/main/java/com/metarealm/metarealm_be/xr/controller/XRController.java
@@ -3,6 +3,9 @@ package com.metarealm.metarealm_be.xr.controller;
 import com.metarealm.metarealm_be.xr.dto.AISTTResponseDto;
 import com.metarealm.metarealm_be.xr.dto.XRTestResponseDto;
 import com.metarealm.metarealm_be.xr.service.XRService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/v1/xr")
+@Slf4j
 public class XRController {
 
     private final XRService xrService;
@@ -20,14 +24,18 @@ public class XRController {
     }
 
     @GetMapping("/test")
-    public XRTestResponseDto testXR() {
-        return XRTestResponseDto.builder()
+    public ResponseEntity<XRTestResponseDto> testXR() {
+        log.info("XR 테스트 메서드 호출");
+        XRTestResponseDto xrTestResponseDto = XRTestResponseDto.builder()
             .testMessage("XR Connection Test")
             .build();
+        return new ResponseEntity<>(xrTestResponseDto, HttpStatus.OK);
     }
 
     @PostMapping("/stt")
     public AISTTResponseDto testAudioFiles(MultipartFile voice) {
-        return xrService.stt(voice);
+        AISTTResponseDto aisttResponseDto = xrService.stt(voice);
+        log.info(aisttResponseDto.toString());
+        return aisttResponseDto;
     }
 }
